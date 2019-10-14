161a36d3cfcddfa5bb7f58d59e8ad94308531b9a
Brian Yap <brian@Brians-MacBook-Air.local>
2019-10-14 15:37:26 +0800

    update fileio

--- fileio.cpp
+++ fileio.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
 int main(){
@@ -7,6 +9,25 @@ int main(){
   MyFile.open("test.txt");
 
   MyFile << "Some text. \n";
+  MyFile << "Another line of text. \n";
+  MyFile << "3 \n";
+  MyFile << "10 \n";
+  MyFile << "2 \n";
   MyFile.close();
+  
+  string line_;
+  ifstream MyFile_("test.txt");
+  if(MyFile_.is_open())
+  {
+      while(getline(MyFile_,line_))
+      {
+	  std::cout << line_ << std::endl;
+      }
+      MyFile_.close();
+  }
+  else {
+      std::cout << "file is not open" << std::endl;
+  }
+  std::cin.get();
   return 0;
 }
